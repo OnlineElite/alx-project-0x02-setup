f33ca715d6f419c03cb3d3828932e1f8fdb6ac77
OnlineElite <online.0.elite@gmail.com>
2025-07-24 23:56:34 +0100

    updating task7

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -3,41 +3,33 @@ import {useState, useEffect} from "react"
 import PostCard from "@/components/common/PostCard"
 import {PostProps} from "@/interfaces"
 
-const Posts: React.FC =()=>{
-
-    const [posts, setPosts] = useState<PostProps[]>([]);
-    const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-        try {
-            const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-            const data: PostProps[] = await res.json();
-            setPosts(data.slice(0, 10)); // Limit to 10 posts
-        } catch (error) {
-            console.error('Error fetching posts:', error);
-        } finally {
-            setLoading(false);
-        }
-    };
-    fetchPosts();
-  }, []);
+const Posts: React.FC =({posts})=>{
 
 
     return(
         <div>
             <Header/>
-            {loading? 
-                ( <div className="flex justify-center items-center mt-[40vh] font-extrabold text-black"><p>Loading posts...</p></div> ) 
-            : 
-                (<div className="list grid grid-cols-4 gap-2 m-3">
-                    {posts.map(({title, body, userId} : PostProps , key : number) =>
-                        <PostCard key={key} title = {title} content= {body} userId={userId}/>
-                    )}
-                </div>)}
+            <div className="list grid grid-cols-4 gap-2 m-3">
+                {posts.map(({title, body, userId} : PostProps , key : number) =>
+                    <PostCard key={key} title = {title} content= {body} userId={userId}/>
+                )}
+            </div>
         </div>
     )
 }
 
+export const getStaticProps  = async () => {
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data: PostProps[] = await res.json();
+        return {
+            props: {posts: data.slice(0, 20)}  // only the first 10
+        };
+  } catch (error) {
+        return {
+            props: { posts: [] }
+        };
+  }
+};
 
 export default Posts;
